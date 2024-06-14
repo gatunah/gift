54322b93395a13c6ed50afa27585208fe50e29fd
Daniela <daanituty@gmail.com>
2024-06-14 02:35:29 -0400

    edicion proyecto

--- script.js
+++ script.js
@@ -14,15 +14,24 @@ function crearAnimaciones() {
 
         const cat = document.createElement('div');
         cat.classList.add('cat');
-        cat.style.backgroundImage = 'url("https://cdn-icons-png.flaticon.com/512/6988/6988878.png")'; // URL de imagen de gatito
+        cat.style.backgroundImage = 'url("https://cdn-icons-png.freepik.com/256/6988/6988878.png?semt=ais_hybrid")'; // URL de la primera imagen de gatito
         cat.style.left = Math.random() * window.innerWidth + 'px';
         cat.style.top = Math.random() * window.innerHeight + 'px';
         cat.style.animationDelay = Math.random() * 5 + 's';
         document.body.appendChild(cat);
 
+        const cat2 = document.createElement('div');
+        cat2.classList.add('cat2');
+        cat2.style.backgroundImage = 'url("https://cdn.icon-icons.com/icons2/2787/PNG/512/black_cat_icon_177458.png")'; // URL de la segunda imagen de gatito
+        cat2.style.left = Math.random() * window.innerWidth + 'px';
+        cat2.style.top = Math.random() * window.innerHeight + 'px';
+        cat2.style.animationDelay = Math.random() * 5 + 's';
+        document.body.appendChild(cat2);
+
         setTimeout(() => {
             heart.remove();
             cat.remove();
+            cat2.remove();
         }, 15000); // Dura más tiempo (15 segundos)
     }
 }
