6f364581b4402c667f8cf00a2516d3a3dcd516f4
Daniela <daanituty@gmail.com>
2024-06-16 01:41:05 -0400

    subida proyecto

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 function mostrarSorpresa() {
     document.getElementById('sorpresa').style.display = 'block';
     crearAnimaciones();
@@ -14,7 +17,7 @@ function crearAnimaciones() {
 
         const cat = document.createElement('div');
         cat.classList.add('cat');
-        cat.style.backgroundImage = 'url("https://cdn-icons-png.freepik.com/256/6988/6988878.png?semt=ais_hybrid")'; // URL de la primera imagen de gatito
+        cat.style.backgroundImage = 'url("https://cdn-icons-png.freepik.com/256/6988/6988878.png?semt=ais_hybrid")'; 
         cat.style.left = Math.random() * window.innerWidth + 'px';
         cat.style.top = Math.random() * window.innerHeight + 'px';
         cat.style.animationDelay = Math.random() * 5 + 's';
@@ -22,7 +25,7 @@ function crearAnimaciones() {
 
         const cat2 = document.createElement('div');
         cat2.classList.add('cat2');
-        cat2.style.backgroundImage = 'url("https://cdn.icon-icons.com/icons2/2787/PNG/512/black_cat_icon_177458.png")'; // URL de la segunda imagen de gatito
+        cat2.style.backgroundImage = 'url("https://cdn.icon-icons.com/icons2/2787/PNG/512/black_cat_icon_177458.png")';
         cat2.style.left = Math.random() * window.innerWidth + 'px';
         cat2.style.top = Math.random() * window.innerHeight + 'px';
         cat2.style.animationDelay = Math.random() * 5 + 's';
@@ -32,6 +35,6 @@ function crearAnimaciones() {
             heart.remove();
             cat.remove();
             cat2.remove();
-        }, 15000); // Dura más tiempo (15 segundos)
+        }, 15000); 
     }
 }
